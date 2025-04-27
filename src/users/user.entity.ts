f921bff1d7f66afaef3b5edc38e9137413e4b8b5
Mate Paić <mate.paic1997@gmail.com>
2025-04-27 12:04:35 +0200

    implemented report logic

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,28 +1,44 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-@Entity()
-export class User {
+import {
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate,
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    OneToMany,
+  } from 'typeorm';
+  import { Report } from '../reports/report.entity';
+  
+  @Entity()
+  export class User {
     @PrimaryGeneratedColumn()
     id: number;
-
+  
     @Column()
     email: string;
-
+  
     @Column()
     password: string;
 
+    @Column({ default: true })
+    admin: boolean;
+  
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
+  
     @AfterInsert()
     logInsert() {
-        console.log('Inserted User with id', this.id);
+      console.log('Inserted User with id', this.id);
     }
-
+  
     @AfterUpdate()
     logUpdate() {
-        console.log('Updated User with id', this.id);
+      console.log('Updated User with id', this.id);
     }
-
+  
     @AfterRemove()
     logRemove() {
-        console.log('Removed User with id', this.id);
+      console.log('Removed User with id', this.id);
     }
-}
+  }
+  
